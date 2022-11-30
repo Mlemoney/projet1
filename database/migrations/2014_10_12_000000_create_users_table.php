1688cee3ad8aa4dd0697a1b275e848b2b4cce7c4
Mlemoney <mervelinlendjoungou@gmail.com>
2022-11-30 15:06:03 +0000

    bonsoir

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('etudianrs', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('nom');
+            $table->string('prenom');
+            $table->date('naissance');
+            $table->string('sexe');
+            $table->integer('telephone');
             $table->timestamps();
         });
     }
